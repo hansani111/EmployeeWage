fee294219e5721e7eaf6a10423dd501e575db173
hansani111 <110657880+hansani111@users.noreply.github.com>
2023-01-15 19:51:17 +0530

    Update EmployeeWage.java

--- src/com/bridgelabz/EmployeeWage.java
+++ src/com/bridgelabz/EmployeeWage.java
@@ -53,8 +53,6 @@ public class EmployeeWage {
 
     }
 
-
-
     public static void main(String[] args) {
         System.out.println("-------Welcome to employee wage computation problem------\n");
 
